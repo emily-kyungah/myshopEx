a6c626842708a766ee42b4ab9d063a1871ab423f
emily <emily.kyungah.kim@gmail.com>
2025-07-10 16:05:09 +0900

    2025-07-10 수업 코드 업로드

---
+++ 2025-07-10/main/java/com/example/myShop/repository/ItemRepository.java
@@ -0,0 +1,29 @@
+package com.example.myShop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.myShop.entity.Item;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.support.QuerydslJpaPredicateExecutor;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item>, ItemRepositoryCustom {
+    List<Item> findByItemName(String itemName);
+
+    List<Item> findByItemNameOrItemDetail(String itemName, String itemDetail);
+
+    List<Item> findByPriceLessThan(Integer price);
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like " +
+            "%:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+    @Query(value="select * from t_item i where i.item_detail like " +
+            "%:itemDetail% order by i.price desc", nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
+
+}
